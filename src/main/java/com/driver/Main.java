704e4e83e09e7e071cb11221d1ea967463d92fe0
Alok <alokkyyadav@gmail.com>
2023-06-09 16:51:03 +0530

    "second push"

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,13 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         Product p= new Product();
-        p.product(1,2);
-        p.product(1,2,3);
-        p.product(1.1,2.2);
+        System.out.println(p.product(1,2));
+        System.out.println(p.product(1,2,3));
+        System.out.println(p.product(1.1,2.2));
     }
     public static class Product{
 
         public int product(int x, int y) {
+
             return x+y;
         }
 
